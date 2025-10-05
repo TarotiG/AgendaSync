0d950104b33cb39d01a91ee356c9c87c30a1203c
TarotiG <tyronlsg@gmail.com>
2025-10-05 09:32:37 +0200

    kleine aanpassing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
     public static void main(String... args) throws IOException, GeneralSecurityException {
         SyncEngine syncEngine = new SyncEngine();
-        var syncEngineGoogleEvents = syncEngine.receiveGoogleEvents();
+        List<SyncEventDto> syncEngineGoogleEvents = syncEngine.receiveGoogleEvents();
 
         // Versturen en vergelijken met DB State
 
